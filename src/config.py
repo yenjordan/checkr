7b178038739c8f57b04b1a594ab91025de93d680
Emily Cho <emilycho@Emilys-MacBook-Pro.local>
2026-02-14 01:52:51 -0500

    config

---
+++ src/config.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+from langchain_core.runnables import RunnableConfig
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    #TODO: update 
+    kb_id: str = ""
+
+settings = Settings()
+
+class PaperCheckerConfig(BaseModel):
+    """base config for paper checker agent lolz we can change name later"""
+    agent_name: str = "Paper Checker"
+    default_prompt: str = "No prompt found in input, please guide user to provide a research publication to check."
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "PaperCheckerConfig":
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+
+        return cls()
